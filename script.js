d7d8e0698c18f68a6d324cd1dbf4a8a56534c646
BALAN ANTONIO FLORIN <12999441@itisrossi.edu.it>
2024-11-28 10:19:13 +0100

    jdgsjgoidsjiogsdj

--- script.js
+++ script.js
@@ -3,6 +3,63 @@ function calcola(op){
     if(op==="="){
         let st = inp.innerHTML;
         st+=" "
+        // ^ e radice
+        for(let i = 0; i<st.length;i++){
+            if(st[i]==="^"){
+                let low;
+                let high;
+                for(let j = i-1;j>=0;j--){
+                    if(isNaN(parseInt(st[j]))){
+                        low = j;
+                        break;
+                    }
+                }
+
+                for(let k = i+1;k<st.length;k++){
+                    if(isNaN(parseInt(st[k]))){
+                        high = k;
+                        break;
+                    }
+                }
+
+                let p1 = (st.substring(low+1,i));
+                let p2 = (st.substring(i+1,high));
+
+                console.log(low);
+                console.log(high);
+
+                console.log(p1);
+                console.log(p2);
+
+                st = st.replace(p1+"^"+p2,Math.pow(parseInt(p1)*parseInt(p2)));
+                console.log(st);
+                i=0;
+            }else if(st[i]==="√"){
+                let low;
+                let high;
+                
+
+                for(let k = i+1;k<st.length;k++){
+                    if(isNaN(parseInt(st[k]))){
+                        high = k;
+                        break;
+                    }
+                }
+
+                let p2 = (st.substring(i+1,high));
+
+                console.log(low);
+                console.log(high);
+
+                //console.log(p1);
+                console.log(p2);
+
+                st = st.replace("√"+p2,Math.sqrt(parseInt(p2)));
+                console.log(st);
+                i=0;
+            }
+        }
+        //* e /
         for(let i = 0; i<st.length;i++){
             if(st[i]==="*"){
                 let low;
